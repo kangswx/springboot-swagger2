cd6e069b756593219c3f60a82d0d96b1fcf8818f
kangswx <996334837@qq.com>
2019-08-08 11:35:27 +0800

    添加注释

--- src/main/java/com/kangswx/springbootswagger2/config/Swagger2.java
+++ src/main/java/com/kangswx/springbootswagger2/config/Swagger2.java
@@ -12,20 +12,29 @@ import springfox.documentation.spring.web.plugins.Docket;
 @Configuration
 public class Swagger2 {
 
+    /**
+     * 生成API对象
+     * @return
+     */
     @Bean
     public Docket createRestApi(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
+                //controller包的路径,生成controller对应的API文档
                 .apis(RequestHandlerSelectors .basePackage("com.kangswx.springbootswagger2.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
 
+    /**
+     * 生成API描述信息
+     * @return
+     */
     public ApiInfo apiInfo(){
         return new ApiInfoBuilder()
                 .title("Spring整合Swagger2的API")
-                .description("简单优雅的restfun风格，https://blog.csdn.net/kangswx")
+                .description("简单优雅的rest风格，https://blog.csdn.net/kangswx")
                 .termsOfServiceUrl("https://blog.csdn.net/kangswx")
                 .version("1.0")
                 .build();
