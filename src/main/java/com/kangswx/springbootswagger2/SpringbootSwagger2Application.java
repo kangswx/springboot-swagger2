b808ac19088e01ac505c03f0ac691b4fea506f1c
kangswx <996334837@qq.com>
2019-08-08 11:35:36 +0800

    添加注释

--- src/main/java/com/kangswx/springbootswagger2/SpringbootSwagger2Application.java
+++ src/main/java/com/kangswx/springbootswagger2/SpringbootSwagger2Application.java
@@ -7,7 +7,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @MapperScan(basePackages = "com.kangswx.springbootswagger2.mapper")
 @SpringBootApplication
-@EnableSwagger2
+@EnableSwagger2     //启动Swagger2
 public class SpringbootSwagger2Application {
 
     public static void main(String[] args) {
